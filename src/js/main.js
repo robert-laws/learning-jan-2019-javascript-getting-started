b1c5644c09e457239ab1e2332694e2a5cc932795
Robert Laws <roblaws@me.com>
2019-01-27 16:59:12 +0300

    add beginning of the blackjack game code

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,16 @@
 // javascript code
+//
+// Blackjack Game
+// by Bob Cobb
+//
 
-console.log('Hello World!');
+// Added with Javascript Basics Section
+
+let card1 = "Ace of Spades",
+    card2 = "Ten of Hearts";
+
+console.log("Welcome to Blackjack");
+
+console.log("You are dealt: ");
+console.log(" " + card1);
+console.log(" " + card2);
