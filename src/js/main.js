bb72e002e5220fe3bef7eaa471fd7277a4252065
Robert Laws <roblaws@me.com>
2019-01-30 21:44:24 +0300

    update main.js file - basic arrays

--- src/js/main.js
+++ src/js/main.js
@@ -6,11 +6,19 @@
 
 // Added with Javascript Basics Section
 
-let card1 = "Ace of Spades",
-    card2 = "Ten of Hearts";
+// let card1 = "Ace of Spades",
+//     card2 = "Ten of Hearts";
+
+let deck = [
+  "Ace of Spades",
+  "Two of Spades",
+  "Three of Spades"
+];
+
+let playerCards = [deck[0], deck[2]];
 
 console.log("Welcome to Blackjack");
 
 console.log("You are dealt: ");
-console.log(" " + card1);
-console.log(" " + card2);
+console.log(" " + playerCards[0]);
+console.log(" " + playerCards[1]);
