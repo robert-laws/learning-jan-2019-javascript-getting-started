e5585724a74729eebb827e4f172fd003af6e43ae
Robert Laws <roblaws@me.com>
2019-02-03 11:11:57 +0300

    add code to main.js and clean up formatting

--- src/js/main.js
+++ src/js/main.js
@@ -18,21 +18,29 @@
 let suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
 let values = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"];
 
-let deck = [];
-
-for (let suitIds = 0; suitIds < suits.length; suitIds++) {
-  for (let valueIds = 0; valueIds < values.length; valueIds++) {
-    deck.push(values[valueIds] + ' of ' + suits[suitIds]);
+function createDeck() {
+  let deck = [];
+  for (let suitIds = 0; suitIds < suits.length; suitIds++) {
+    for (let valueIds = 0; valueIds < values.length; valueIds++) {
+      deck.push(values[valueIds] + ' of ' + suits[suitIds]);
+    }
   }
+  return deck;
 }
+
+function getNextCard() {
+  return deck.shift();
+}
+
+let deck = createDeck();
 
 // for (let i = 0; i < deck.length; i++) {
 //   console.log(deck[i]);
 // }
 
-console.log(deck.length);
+// let playerCards = [deck[0], deck[2]];
 
-let playerCards = [deck[0], deck[2]];
+let playerCards = [getNextCard(), getNextCard()];
 
 console.log("Welcome to Blackjack");
 
