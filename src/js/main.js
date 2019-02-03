e648ece2488d6b5bc114c78e0306ae98d00ef92a
Robert Laws <roblaws@me.com>
2019-02-03 07:44:13 +0300

    update main.js file - create deck of cards with for loop

--- src/js/main.js
+++ src/js/main.js
@@ -9,11 +9,28 @@
 // let card1 = "Ace of Spades",
 //     card2 = "Ten of Hearts";
 
-let deck = [
-  "Ace of Spades",
-  "Two of Spades",
-  "Three of Spades"
-];
+// let deck = [
+//   "Ace of Spades",
+//   "Two of Spades",
+//   "Three of Spades"
+// ];
+
+let suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
+let values = ["Ace", "King", "Queen", "Jack", "Ten", "Nine", "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"];
+
+let deck = [];
+
+for (let suitIds = 0; suitIds < suits.length; suitIds++) {
+  for (let valueIds = 0; valueIds < values.length; valueIds++) {
+    deck.push(values[valueIds] + ' of ' + suits[suitIds]);
+  }
+}
+
+// for (let i = 0; i < deck.length; i++) {
+//   console.log(deck[i]);
+// }
+
+console.log(deck.length);
 
 let playerCards = [deck[0], deck[2]];
 
